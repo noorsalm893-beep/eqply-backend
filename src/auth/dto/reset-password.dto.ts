5a080efa75bb20b3d6294568fa2f04e4b58e1ede
Mohamed Elshamy <mohamedabdelhady88@gmail.com>
2026-03-16 20:26:40 +0200

    swagger

--- src/auth/dto/reset-password.dto.ts
+++ src/auth/dto/reset-password.dto.ts
@@ -1,10 +1,13 @@
 import { IsString, IsNotEmpty, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class ResetPasswordDto {
+  @ApiProperty({ example: 'reset-token' })
   @IsString()
   @IsNotEmpty()
   token: string;
 
+  @ApiProperty({ minLength: 8, example: 'N3wP@ssw0rd!' })
   @IsString()
   @MinLength(8)
   newPassword: string;
