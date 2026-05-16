0c71b48a4200704f0b2df39c88f3bdaf1209bb7c
noorsalm893-beep <noorsalm893@gmail.com>
2026-05-16 23:20:17 +0300

    .

--- src/cart/cart.controller.js
+++ src/cart/cart.controller.js
@@ -21,6 +21,19 @@ var __decorate =
 var __metadata =
   (this && this.__metadata) ||
   function (k, v) {
+    if (typeof Reflect === 'object' && typeof Reflect.metadata === 'function')
+      return Reflect.metadata(k, v);
+  };
+var __param =
+  (this && this.__param) ||
+  function (paramIndex, decorator) {
+    return function (target, key) {
+      decorator(target, key, paramIndex);
+    };
+  };
+
+  (this && this.__metadata) ||
+  function (k, v) {
     if (typeof Reflect === 'object' && typeof Reflect.decorate === 'function')
       return Reflect.metadata(k, v);
   };
@@ -88,7 +101,8 @@ __decorate(
     (0, common_1.Post)('add'),
     (0, common_1.UseGuards)(jwt_auth_guard_1.JwtAuthGuard),
     (0, swagger_1.ApiBearerAuth)(),
-    (0, common_1.Body)(),
+    __param(0, (0, current_user_decorator_1.CurrentUser)()),
+    __param(1, (0, common_1.Body)()),
     (0, common_1.HttpCode)(common_1.HttpStatus.OK),
     __metadata('design:type', Function),
     __metadata('design:paramtypes', [
@@ -106,7 +120,8 @@ __decorate(
     (0, common_1.Post)('remove'),
     (0, common_1.UseGuards)(jwt_auth_guard_1.JwtAuthGuard),
     (0, swagger_1.ApiBearerAuth)(),
-    (0, common_1.Body)(),
+    __param(0, (0, current_user_decorator_1.CurrentUser)()),
+    __param(1, (0, common_1.Body)()),
     (0, common_1.HttpCode)(common_1.HttpStatus.OK),
     __metadata('design:type', Function),
     __metadata('design:paramtypes', [
@@ -124,6 +139,7 @@ __decorate(
     (0, common_1.Get)(),
     (0, common_1.UseGuards)(jwt_auth_guard_1.JwtAuthGuard),
     (0, swagger_1.ApiBearerAuth)(),
+    __param(0, (0, current_user_decorator_1.CurrentUser)()),
     (0, common_1.HttpCode)(common_1.HttpStatus.OK),
     __metadata('design:type', Function),
     __metadata('design:paramtypes', [Object]),
@@ -138,6 +154,7 @@ __decorate(
     (0, common_1.Post)('clear'),
     (0, common_1.UseGuards)(jwt_auth_guard_1.JwtAuthGuard),
     (0, swagger_1.ApiBearerAuth)(),
+    __param(0, (0, current_user_decorator_1.CurrentUser)()),
     (0, common_1.HttpCode)(common_1.HttpStatus.OK),
     __metadata('design:type', Function),
     __metadata('design:paramtypes', [Object]),
