7d5e82970937a1f148caeadabe9bc1ba93eeda38
noorsalm893-beep <noorsalm893@gmail.com>
2026-05-25 12:10:57 +0300

    0

--- src/payment/Payments.controller.js
+++ src/payment/Payments.controller.js
@@ -15,7 +15,8 @@ Object.defineProperty(exports, '__esModule', { value: true });
 exports.PaymentsController = void 0;
 const common_1 = require('@nestjs/common');
 const swagger_1 = require('@nestjs/swagger');
-const payments_controller_1 = require('./Payments.controller');
+const payments_service_1 = require('./Payments.service');
+const jwt_auth_guard_1 = require('../common/guards/jwt-auth.guard');
 const current_user_decorator_1 = require('../common/decorators/current-user.decorator');
 
 let PaymentsController = class PaymentsController {
